8330a274da30b771981351e88409798387493eb9
Akmaral Tursunbaeva <akmaralmt20@gmail.com>
2026-08-18 17:10:07 +0600

    Initial commit

---
+++ src/main/java/pcodes/jpaproject/ems/entity/Employee.java
@@ -0,0 +1,71 @@
+package pcodes.jpaproject.ems.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String department;
+    private Double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String department, Double salary, String name, Long id) {
+        this.department = department;
+        this.salary = salary;
+        this.name = name;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
---
+++ src/main/java/pcodes/jpaproject/ems/model/EmployeeAddResponse.java
@@ -0,0 +1,30 @@
+package pcodes.jpaproject.ems.model;
+
+public class EmployeeAddResponse {
+    private String name;
+    private Long id;
+
+    public EmployeeAddResponse() {
+    }
+
+    public EmployeeAddResponse(String name, Long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
